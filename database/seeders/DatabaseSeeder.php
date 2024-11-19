070a08e9f5406d869e19c767d996e2fa87edc60d
FranMunozGlez <franciscomgz11@gmail.com>
2024-11-19 13:28:59 +0100

    feature: added seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Journal;
 
 class DatabaseSeeder extends Seeder
 {
