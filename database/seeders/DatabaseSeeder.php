3e873f2162001d16e6869351775e7a2c54f01d94
FranMunozGlez <franciscomgz11@gmail.com>
2024-11-19 15:19:53 +0100

    fix: add faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-    
+        Journal::factory(10)->create();
     }
 }
