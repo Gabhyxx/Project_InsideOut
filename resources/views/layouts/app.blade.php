9508e9f3357f07c0ac9b10e168a59e7e33dc7b56
Gabhyxx <jgabrielpg00@gmail.com>
2024-11-19 15:53:39 +0100

    fix: syntax error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Mi Aplicacion'))/title>
+    <title>@yield('title', 'Mi Aplicacion')/title>
 </head>
 <body>
     <div id="app">
@@ -16,6 +16,6 @@
         <x-footer />
     </div>
 
-    <script src="{{ asset('js/app.js}}"></script>
+    <script src="{{ asset('js/app.js')}}"></script>
 </body>
 </html>
