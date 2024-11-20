41e7b02f9516010cfe9b6cf6fdbaf20b0fdb293c
Camposx <alejandrocamposgon@gmail.com>
2024-11-21 00:16:04 +0100

    feat: Create example card for every diary entry

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,4 +21,17 @@
         </tbody>
       </table>
 </div>
+
+<div class="diaryCard">
+  <div>
+    <p class="cardEntry">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat nulla libero soluta fugiat excepturi! Hic veniam ab laudantium amet! Ullam deleniti totam ex optio quidem ea quaerat minima magnam eligendi?</p>
+    <div>
+      <p>Created at:</p>
+      <div class="dateTime"></div>
+    </div>
+  </div>
+
+  <img class="emotionsImg" src="{{ asset('css/joy.png')}}" alt="">
+</div>
+
 @endsection
