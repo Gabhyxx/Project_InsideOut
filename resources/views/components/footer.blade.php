ff3cbe1fc7a0e2571456644972d566dcb0e3c59b
Camposx <alejandrocamposgon@gmail.com>
2024-11-21 00:15:36 +0100

    feat: add footer sections and github names

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,42 @@
 <footer>
-    <h5>
-        Hola Wenas :3
-    </h5>
+
+    <div class="firstFooterSection">
+        <div class="footerLogoSection">
+            <img src="" alt="">
+        </div>
+    
+        <div class="githubSection">
+            <a href="" class="github">
+                <img src="" alt="">
+                <h4>Camposx</h4>
+            </a>
+
+            <a href="" class="github">
+                <img src="" alt="">
+                <h4>Crudo7</h4>
+            </a>
+
+            <a href="" class="github">
+                <img src="" alt="">
+                <h4>Gabhyxx</h4>
+            </a>
+
+            <a href="" class="github">
+                <img src="" alt="">
+                <h4>Mangel1111</h4>
+            </a>
+
+            <a href="" class="github">
+                <img src="" alt="">
+                <h4>Matias</h4>
+            </a>
+        </div>
+    </div>
+
+    <div class="copySection">
+        <p>© 2024 Team 3. All rights reserved.</p>
+        <p>Site by Team 3</p>
+    </div>
+
 </footer>
 
