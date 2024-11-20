7a7a7d8f4425bda9352a1245d70786314118978d
FranMunozGlez <franciscomgz11@gmail.com>
2024-11-20 13:01:03 +0100

    fix:fixed views problems

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<h3>contenido</h3>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Mi Aplicacion')/title>
+    <title>@yield('title', 'Mi Aplicacion')</title>
 </head>
 <body>
     <div id="app">
@@ -14,7 +14,7 @@
         
         <x-footer />
     </div>
-
+    <script src="{{ asset('js/app.js')}}"></script>
     
 </body>
 </html>
