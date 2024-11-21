59862ebe43f1a526dc5287ddf63a7429d60dc83f
Camposx <alejandrocamposgon@gmail.com>
2024-11-21 22:19:47 +0100

    style: make the card bigger for show page and add buttom for going back

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- <div class="details" id="cardShow">
-        <img src="" alt="{{ $journal->emotion }}">
-        <h4>{{ $journal->created_at }}</h4>
-        <p>{{ $journal->entry }}</p>
-        <a href="{{ route('home') }}" class="btn btn-secondary">Back</a>
-    </div> --}}
 
-    <div class="diaryCard">
-        <div class="entryAndDate">
-          <p class="cardEntry">{{ $journal->entry}}</p>
-          <div class="bothDateTime">
-            <div>
-              <p>Created at:</p>
-              <div class="dateTime">{{ $journal->created_at}}</div>
+    <div class="detailedCard">
+        <div class="diaryCard">
+            <div class="entryAndDate">
+              <p class="cardEntry">{{ $journal->entry}}</p>
+              <div class="bothDateTime">
+                <div>
+                  <p>Created at:</p>
+                  <div class="dateTime">{{ $journal->created_at}}</div>
+                </div>
+                <div>
+                  <p>Updated at:</p>
+                  <div class="dateTime">{{ $journal->updated_at}}</div>
+                </div>
+              </div>
             </div>
-            <div>
-              <p>Updated at:</p>
-              <div class="dateTime">{{ $journal->updated_at}}</div>
-            </div>
-          </div>
+          
+            <img class="emotionsImg" src="{{ asset($journal->emotion) }}" alt="Inside Out emotion">
         </div>
-      
-        <img class="emotionsImg" src="{{ asset($journal->emotion) }}" alt="Inside Out emotion">
     </div>
+    <div class="goBackSection">
+        <a href="{{ route('home')}}"><button>Go Back</button></a>
+    </div>
+    
 @endsection
