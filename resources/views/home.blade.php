dd6db07bdbdda9a968dd2f4f10b405d3f21ba035
Camposx <alejandrocamposgon@gmail.com>
2024-11-21 13:12:25 +0100

    fix: change journal card instead of table row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,27 +2,26 @@
 
 @section('content')
 <div>
-    <table class="table table-striped table-bordered border-primary">
-        <thead>
-          <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Entry</th>
-            <th scope="col">Emotions</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach($journals as $journal)
-                <tr>
-                    <td>{{ $journal->created_at}}</td>
-                    <td>{{ $journal->entry}}</td>
-                    <td><img src="{{ asset($journal->emotion) }}" class="imageEmotion" alt=""></td>
-                </tr>
-            @endforeach
-        </tbody>
-      </table>
+  @foreach($journals as $journal)
+    <a href="" class="diaryCard">
+      <div>
+        <p class="cardEntry">{{ $journal->entry}}</p>
+        <div>
+          <p>Created at:</p>
+          <div class="dateTime">{{ $journal->created_at}}</div>
+        </div>
+        <div>
+          <p>Updated at:</p>
+          <div class="dateTime">{{ $journal->updated_at}}</div>
+        </div>
+      </div>
+    
+      <img class="emotionsImg" src="{{ asset($journal->emotion) }}" alt="Inside Out emotion">
+    </a>
+  @endforeach
 </div>
 
-<div class="diaryCard">
+{{-- <a href="" class="diaryCard">
   <div>
     <p class="cardEntry">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat nulla libero soluta fugiat excepturi! Hic veniam ab laudantium amet! Ullam deleniti totam ex optio quidem ea quaerat minima magnam eligendi?</p>
     <div>
@@ -31,7 +30,8 @@
     </div>
   </div>
 
-  <img class="emotionsImg" src="https://res.cloudinary.com/djdpt7dqi/image/upload/v1732179767/joy.png" alt="">
-</div>
+  <img class="emotionsImg" src="https://res.cloudinary.com/djdpt7dqi/image/upload/v1732179766/envy1.png" alt="">
+</a> --}}
+
 
 @endsection
