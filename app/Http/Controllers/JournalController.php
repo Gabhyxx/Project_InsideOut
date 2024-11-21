37105fef0c34caadb6959f4f41f3a34e4c099fb2
Camposx <alejandrocamposgon@gmail.com>
2024-11-21 20:17:43 +0100

    feat: add show method for blade with route and .blade file

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -19,18 +19,18 @@ class JournalController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+    //     //
+    // }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
+    // /**
+    //  * Store a newly created resource in storage.
+    //  */
+    // public function store(Request $request)
+    // {
 
-    }
+    // }
 
     /**
      * Display the specified resource.
@@ -38,29 +38,31 @@ class JournalController extends Controller
     public function show(string $id)
     {
         //
+        $journal = Journal::find($id);
+        return view('show', compact('journal'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
-    }
+    // public function edit(string $id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, string $id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(string $id)
+    // {
+    //     //
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JournalController;
 
 Route::get('/', [JournalController::class, "index"])->name("home") ;
+Route::get('/journals/{id}', [JournalController::class, 'show'])->name('showOneJournal');
