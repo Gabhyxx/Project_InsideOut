f3f63251b26413e36daf8bfc20a9dd2192237724
Mangel111111111 <mangel111111111@gmail.com>
2024-11-21 15:08:39 +0100

    test: add CheckIfCanCreateNewEntryInJournalCheckWithJsonFile

--- tests/Feature/JournalTest.php
+++ tests/Feature/JournalTest.php
@@ -20,4 +20,31 @@ class JournalTest extends TestCase
         $response->assertStatus(200)
             ->assertViewIs('home');
     }
+    public function test_CheckIfReceiveAllEntryOfJournalInJsonFile(){
+        $journal = Journal::factory(2)->create();
+
+        $response = $this->get(route('apihome'));
+
+        $response->assertStatus(200)
+                ->assertJosonCount(2);
+    }
+    public function test_CheckIfCanDeleteEntryinJournalWithApi(){
+        $journal = Journal::factory(2)->create();
+
+        $response = $this->delete(route('apidestroy, 1'));
+        $this->assertDatabaseConunt('journals', 1);
+
+        $response = $this->get(route('apihome'));
+        $response->assertJsonCount(1);
+    }
+    public function test_CheckIfCanCreateNewEntryInJournalCheckWithJsonFile(){
+        $response = $this->post(route('apistore'), [
+            'emtry' => 'Hola mundo',
+            'emotion' => 'Emocion de viva el mundo'
+        ]);
+
+        $response = $this->get(route('apihome'));
+        $response->assertStatus(200)
+                ->assertJsonCount(1);
+    }
 }
