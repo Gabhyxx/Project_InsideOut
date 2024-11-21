317a52b7ef4d410810c3f435504c39a439df6c6a
Mangel111111111 <mangel111111111@gmail.com>
2024-11-21 14:36:12 +0100

    feat: add public function store

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\Api\JournalController;
 
 Route::get('/journals',[JournalController::class,'index'])->name('apihome');
 Route::delete('/journals/{id}',[JournalController::class,'destroy'])->name('apidestroy');
+Route::post('/journals', [JournalController::class,''])->name('');
