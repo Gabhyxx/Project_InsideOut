7f28e70bee66686605fcc10be440610bf3baee02
Camposx <alejandrocamposgon@gmail.com>
2024-11-21 12:50:56 +0100

    style: add style for static and dynamic header with logos

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,4 @@
 <header>
-    <h1>HOLA MUNDO!!</h1>
+    <img src="https://res.cloudinary.com/djdpt7dqi/image/upload/v1732179767/railey-logo.png" alt="Riley shadow with emotions in her head" class="rileyLogo">
+    <img src="https://res.cloudinary.com/djdpt7dqi/image/upload/v1732179766/inside-out-logo.png" alt="Inside Out logo" class="headerLogo">
 </header>
